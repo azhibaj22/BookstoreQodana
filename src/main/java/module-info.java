f32371bad86ee98b5fa2e2816365152edfb3b898
azhgiz <ana.zhibaj@giz.de>
2024-12-22 22:41:56 +0100

    Added all files for qodana test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module main {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens main to javafx.fxml;
+    exports main;
+}
